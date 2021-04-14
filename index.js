80ce083684d9152e8a6451e22ecc6c7791570c9e
bruce mcpherson <bruce@mcpher.com>
2021-04-14 13:40:41 +0100

    v1.1.0

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const {
   CrusherPluginUpstashService,
 } = require("./src/CrusherPluginUpstashService");
+const {
+  CrusherPluginGitService
+}= require("./src/CrusherPluginGitService");
 module.exports = {
   CrusherPluginUpstashService,
+  CrusherPluginGitService,
 };
